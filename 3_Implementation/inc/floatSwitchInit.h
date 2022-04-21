a3fe8017dd00d5d5f54486064b92f83d8e39dcc5
Akash Rathod <101167678+akashrathod3407@users.noreply.github.com>
2022-04-21 17:30:05 +0530

    Add files via upload

---
+++ 3_Implementation/inc/floatSwitchInit.h
@@ -0,0 +1,24 @@
+#ifndef _FLOATSWITCHINIT_
+#define _FLOATSWITCHINIT_
+/**
+ * @file floatSwitchInit.h
+ * @author Mohanraj D
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+/*
+*Includes files
+*/
+#include <avr/io.h>
+/**
+ * Function defenition
+ */
+void floatswitchInit(void);
+
+#endif
---
+++ 3_Implementation/inc/relayInit.h
@@ -0,0 +1,23 @@
+#ifndef _REALAYINIT_
+#define _REALAYINIT_
+/**
+ * @file relayInit.h
+ * @author Mohanraj D
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+/**
+ * Incleding files 
+*/
+#include <avr/io.h>
+/**
+ * @brief Function decleration
+ * 
+ */
+void relayInit(void);
+
+#endif
